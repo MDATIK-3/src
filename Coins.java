73f43f0425523be916e7f84df675b6cad4844ef2
Atik Hasan <atikh4335@gmail.com>
2024-01-05 16:11:12 +0600

    1300 rating

---
+++ Coins.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Coins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+//        System.out.printf("%d ", n);
+
+        while (n > 0) {
+            if ( n % 2 == 0 ) {
+                System.out.printf("%d ", n);
+                n /= 2;
+            } else {
+                for (long i = 3; i <= n; i++) {
+                    if ( n % i == 0 ) {
+                        System.out.printf("%d ", n);
+                        n /= i;
+                        i=2;
+                    }
+                }
+
+            }
+            if ( n == 1 ) {
+                System.out.printf("%d\n", 1);
+                break;
+            }
+        }
+
+
+    }
+}
