0d099de6c6f17ba0971f3e25c3f634c23418e7ed
Atik Hasan <atikh4335@gmail.com>
2024-01-05 14:35:07 +0600

    matrix

--- Beautiful_Matrix.java
+++ Beautiful_Matrix.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Beautiful_Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum1 = 0, sum2 = 0;
+        int arr[][] = new int[ 5 ][ 5 ];
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++) {
+
+                arr[ i ][ j ] = sc.nextInt();
+                if ( arr[ i ][ j ] == 1 ) {
+                    sum1 = Math.abs(2 - i);
+                    sum2 = Math.abs(2 - j);
+                    System.out.println(sum1 + sum2);
+                    return;
+                }
+            }
+    }
+
 }
